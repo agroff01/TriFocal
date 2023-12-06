8791ff05232080325208801321b7d6fa562ae995
starrami00 <starrami@ucsc.edu>
2023-12-05 21:46:08 -0800

    Scene Transitions Done

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneTransition : MonoBehaviour
+{
+    public Image fadeImage;
+    public float fadeDuration = 1.0f;
+    public float delayBeforeTransition = 35.0f; // Set the delay time in seconds
+
+    private void Start()
+    {
+        // Start the fade-in effect when the scene loads
+        StartCoroutine(FadeIn());
+    }
+
+    private void Update()
+    {
+        // Check if the delayBeforeTransition time has passed, and start the transition if it has
+        if (Time.timeSinceLevelLoad >= delayBeforeTransition)
+        {
+            TransitionToScene("GameScene");
+        }
+    }
+
+    IEnumerator FadeIn()
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration);
+            fadeImage.color = new Color(0f, 0f, 0f, alpha);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOut(string sceneName)
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            float alpha = Mathf.Lerp(0f, 1f, elapsedTime / fadeDuration);
+            fadeImage.color = new Color(0f, 0f, 0f, alpha);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // Load the target scene after the fade-out
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void TransitionToScene(string sceneName)
+    {
+        StartCoroutine(FadeOut(sceneName));
+    }
+}
