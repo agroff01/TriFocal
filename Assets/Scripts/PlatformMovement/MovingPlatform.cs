dafb083454006117565e684778d22517eecd883e
starrami00 <starrami@ucsc.edu>
2023-12-06 01:32:10 -0800

    Organizng scripts & Adding moving platform script

---
+++ Assets/Scripts/PlatformMovement/MovingPlatform.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+// Pathway and Waypoint Logic for moving platforms followed from this video: https://www.youtube.com/watch?v=ly9mK0TGJJo&ab_channel=KetraGames
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private WaypointPath _waypointPathway;
+
+    [SerializeField]
+    private float _speed;
+
+    private int _targetWaypointIndex;
+
+    private Transform _previousPoint;
+    private Transform _targetPoint;
+
+    private float _timeToWaypoint;
+    private float _elapsedTime;
+
+    private void TargetNextWaypoint()
+    {
+        _previousPoint = _waypointPathway.GetWaypoint(_targetWaypointIndex);
+        _targetWaypointIndex = _waypointPathway.GetNextWaypointIndex(_targetWaypointIndex);
+        _targetPoint = _waypointPathway.GetWaypoint(_targetWaypointIndex);
+
+        _elapsedTime = 0;
+
+        float distanceToWaypoint = Vector3.Distance(_previousPoint.position, _targetPoint.position);
+        _timeToWaypoint = distanceToWaypoint / _speed;
+    }
+
+    private void Start()
+    {
+        TargetNextWaypoint();
+    }
+
+    private void FixedUpdate()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        float elapsedPercent = _elapsedTime / _timeToWaypoint;
+        transform.position = Vector3.Lerp(_previousPoint.position, _targetPoint.position, elapsedPercent);
+
+        if (elapsedPercent >= 1)
+        {
+            TargetNextWaypoint();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player") other.transform.SetParent(transform);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player") other.transform.SetParent(null);
+    }
+}
---
+++ Assets/Scripts/PlatformMovement/WaypointPath.cs
@@ -0,0 +1,26 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Pathway and Waypoint Logic for moving platforms followed from this video: https://www.youtube.com/watch?v=ly9mK0TGJJo&ab_channel=KetraGames
+
+
+public class WaypointPath : MonoBehaviour
+{
+
+    public Transform GetWaypoint(int waypointIndex)
+    {
+        return transform.GetChild(waypointIndex);
+    }
+
+    public int GetNextWaypointIndex(int currentWaypointIndex)
+    {
+        int nextWaypointIndex = currentWaypointIndex + 1;
+        if (nextWaypointIndex == transform.childCount)
+        {
+            nextWaypointIndex = 0;
+        }
+        return nextWaypointIndex;
+    }
+}
