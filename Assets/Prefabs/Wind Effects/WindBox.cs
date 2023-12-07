9432e8f221eba6236c8d8ff7c829dff7ab73704b
Yasha Bell <zbell@ucsc.edu>
2023-12-07 14:04:49 -0800

    Wind jumps added

--- Assets/Prefabs/Wind Effects/WindBox.cs
+++ Assets/Prefabs/Wind Effects/WindBox.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class WindBox : MonoBehaviour
 {
-    public PlayerMovement PM;
     public float windForce;
     public Transform windBase;
-    public Transform playerPos;
-    public Rigidbody player;
-    public ParticleSystem speedLines;
-
     /*public void OnTriggerStay(Collider collid)
     {
         if (collid.attachedRigidbody)
@@ -25,14 +20,9 @@ public class WindBox : MonoBehaviour
     {
         if (collid.attachedRigidbody && collid.CompareTag("Player"))
         {
-            speedLines.Play();
+            
             collid.attachedRigidbody.AddForce(windBase.up * windForce);
         }
-    }
-
-    public void OnTriggerExit()
-    {
-        speedLines.Stop();
     }
 }
    
