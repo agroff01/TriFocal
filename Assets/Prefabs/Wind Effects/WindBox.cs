40e459ca16b7daf31ce97e4031dc1ebb302573b8
FinnDePuy <103275480+FinnDePuy@users.noreply.github.com>
2023-12-08 11:18:43 -0800

    fixed errors, and got rid of extra glasses.

--- Assets/Prefabs/Wind Effects/WindBox.cs
+++ Assets/Prefabs/Wind Effects/WindBox.cs
@@ -10,7 +10,7 @@ public class WindBox : MonoBehaviour
 
     public void OnTriggerStay(Collider collid)
     {
-        if (collid.attachedRigidbody)
+        if (collid.attachedRigidbody && collid.tag == "Player")
         {
             collid.attachedRigidbody.velocity = windBase.up * windForce;
         }
