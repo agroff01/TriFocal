322ef6644f8a3a810ddfa05dc2eaca4ac2fdcaa1
FinnDePuy <103275480+FinnDePuy@users.noreply.github.com>
2023-12-08 10:46:35 -0800

    pushing for testing.

--- Assets/EndingText.cs
+++ Assets/EndingText.cs
@@ -7,18 +7,24 @@ using System;
 
 public class EndingText : MonoBehaviour
 {
+    public TMP_Text textDisplay;
+    public float delayBetweenTexts = 1f; // Delay between each text display
+
+    public string[] texts;
+    private int currentIndex = 0;
+
     public ColorManager ColorManager;
-    public TMP_Text first;
-    public TMP_Text second;
-    public TMP_Text third;
-    public TMP_Text fourth;
+    // public TMP_Text first;
+    // public TMP_Text second;
+    // public TMP_Text third;
+    // public TMP_Text fourth;
     // Start is called before the first frame update
     void Start()
     {
-        first.enabled = !first.enabled;
-        second.enabled = !second.enabled;
-        third.enabled = !third.enabled;
-        fourth.enabled = !fourth.enabled;
+        // first.enabled = !first.enabled;
+        // second.enabled = !second.enabled;
+        // third.enabled = !third.enabled;
+        // fourth.enabled = !fourth.enabled;
     }
 
     // Update is called once per frame
@@ -26,7 +32,26 @@ public class EndingText : MonoBehaviour
     {
         if(ColorManager.allLensesCollected)
         {
-
+            
         }
+    }
+
+
+
+    // private void Start()
+    // {
+    //     StartCoroutine(PrintTexts());
+    // }
+
+    private IEnumerator PrintTexts()
+    {
+        foreach (string textToDisplay in texts)
+        {
+            textDisplay.text = textToDisplay;
+            yield return new WaitForSeconds(delayBetweenTexts);
+        }
+
+        // Optional: Do something after all texts are displayed
+        Debug.Log("All texts displayed");
     }
 }
