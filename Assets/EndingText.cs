3393d8313b714d2507911b1871ec098eb9427703
FinnDePuy <103275480+FinnDePuy@users.noreply.github.com>
2023-12-08 11:13:36 -0800

    adding the text script

--- Assets/EndingText.cs
+++ Assets/EndingText.cs
@@ -1,43 +1,65 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
-using System;
+using UnityEngine;
 
 public class EndingText : MonoBehaviour
 {
-    public float delayBetweenTexts = 1f; // Delay between each text display
-
+    public float delayBetweenTexts = 3.0f;
     public TMP_Text[] text;
     private int currentIndex = 0;
+    private bool flag = false;
 
     public ColorManager ColorManager;
 
     void Start()
     {
-        text[0].enabled = !text[0].enabled;
-        text[1].enabled = !text[1].enabled;
-        text[2].enabled = !text[2].enabled;
-        text[3].enabled = !text[3].enabled;
+        InitializeText();
     }
 
-    // Update is called once per frame
+    void InitializeText()
+    {
+        foreach (var txt in text)
+        {
+            Color textColor = txt.color;
+            textColor.a = 0f; // Set alpha to 0 for initial fade-in
+            txt.color = textColor;
+        }
+    }
+
     void Update()
     {
-        if(ColorManager.allLensesCollected)
+        if (ColorManager.allLensesCollected && !flag)
         {
             StartCoroutine(PrintTexts());
+            flag = true;
         }
     }
 
     private IEnumerator PrintTexts()
     {
-        while(currentIndex < 4)
+        while (currentIndex < 4)
         {
-            text[currentIndex].enabled = !text[currentIndex].enabled;
-            yield return new WaitForSeconds(delayBetweenTexts);
+            yield return StartCoroutine(FadeText(text[currentIndex], 1f, delayBetweenTexts / 2f)); // Fade in
+            yield return new WaitForSeconds(delayBetweenTexts / 2f); // Wait for half of the delay
+            yield return StartCoroutine(FadeText(text[currentIndex], 0f, delayBetweenTexts / 2f)); // Fade out
             currentIndex++;
         }
+    }
+
+    IEnumerator FadeText(TMP_Text txt, float targetAlpha, float duration)
+    {
+        Color textColor = txt.color;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            textColor.a = Mathf.Lerp(textColor.a, targetAlpha, elapsedTime / duration);
+            txt.color = textColor;
+            yield return null;
+        }
+
+        textColor.a = targetAlpha; // Ensure alpha is set to the target value
+        txt.color = textColor;
     }
 }
